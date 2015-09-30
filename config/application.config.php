1ccab7bf5c81e1146a236565468edc3032655e20
Derek <Derek@api.carsite.local>
2015-09-29 21:58:20 -0700

    CORS configuration

--- config/application.config.php
+++ config/application.config.php
@@ -21,6 +21,7 @@ return array(
         'ZF\\Rpc',
         'ZF\\Versioning',
         'ZF\\DevelopmentMode',
+        'ZfrCors',
         'Carsite',
     ),
     'module_listener_options' => array(
